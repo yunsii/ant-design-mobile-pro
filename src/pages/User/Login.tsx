9f7a937435e422e3f7e457b8358c034d5c8b8fbd
xieyunsheng <xieyunsheng>
2019-08-13 10:03:20 +0800

    feat: iconfontUrl

--- src/pages/User/Login.tsx
+++ src/pages/User/Login.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { List, InputItem } from 'antd-mobile';
 import Paper from '@/components/Paper';
+import CustomIcon from '@/components/CustomIcon';
 import { Button } from '@/components/CustomAntdMobile';
 import styles from './Login.less';
 
 const logo = '/logo.svg';
-
 const logoStyle = {
   position: 'relative',
   width: 48,
@@ -19,6 +19,8 @@ const logoStyle = {
   // backgroundSize: '48px 48px',
 }
 
+const iconStyle = { fontSize: 24, color: '#776e6e' };
+
 export default class extends React.PureComponent {
   render() {
     const icon = (
@@ -31,14 +33,29 @@ export default class extends React.PureComponent {
         />
       </div>
     );
+
+
     return (
       <div style={{ marginTop: 'calc(50vh - 180px)' }}>
         <Paper icon={icon}>
           <List style={{ marginTop: 32 }}>
-            <InputItem placeholder="请输入帐号" autoComplete='off' />
-            <InputItem type="password" placeholder="请输入密码" autoComplete='off' />
+            <InputItem
+              placeholder="请输入帐号"
+              autoComplete='off'
+              labelNumber={1}
+            >
+              <CustomIcon type="user" style={iconStyle} />
+            </InputItem>
+            <InputItem
+              type="password"
+              placeholder="请输入密码"
+              autoComplete='off'
+              labelNumber={1}
+            >
+              <CustomIcon type="password" style={iconStyle} />
+            </InputItem>
           </List>
-          <Button type="primary" style={{ margin: '32px 0' }}>
+          <Button type="primary" style={{ margin: '32px 4px' }}>
             登录
           </Button>
         </Paper>
