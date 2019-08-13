4acde0db156b56fe233d0bb2776b24d220fe8fae
Yuns Xie <yuns.xie@qq.com>
2019-08-13 21:51:19 +0800

    feat: footer

--- src/layouts/UserLayout.tsx
+++ src/layouts/UserLayout.tsx
@@ -1,13 +1,21 @@
 // import DocumentTitle from 'react-document-title';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'dva';
+import {} from 'antd-mobile'
 // import { formatMessage } from 'umi-plugin-react/locale';
-
+import Footer from './Footer';
+import CustomIcon from '@/components/CustomIcon';
 import { ConnectState } from '@/models/connect';
 import styles from './UserLayout.less';
 
 export interface UserLayoutProps {
 }
+
+const copyright = (
+  <Fragment>
+    Copyright <CustomIcon type="copyright" /> 2019 <a href="https://github.com/theprimone">theprimone</a> 出品
+  </Fragment>
+);
 
 const UserLayout: React.SFC<UserLayoutProps> = props => {
   const {
@@ -19,6 +27,7 @@ const UserLayout: React.SFC<UserLayoutProps> = props => {
       <div className={styles.content}>
         {children}
       </div>
+      <Footer copyright={copyright} />
     </div>
   );
 };
