1eb713c40ac2166988782eda316405be06195b9c
xieyunsheng <xieyunsheng>
2019-08-13 10:02:55 +0800

    feat: addScriptToHead

---
+++ src/global.ts
@@ -0,0 +1,11 @@
+const defaultSettings = require('./defaultSettings');
+const { iconfontUrl } = defaultSettings;
+
+function addScriptToHead(url: string) {
+  const head = document.getElementsByTagName('head')[0];
+  const script = document.createElement('script');
+  script.src= url;
+  head.appendChild(script);
+}
+
+addScriptToHead(iconfontUrl);
