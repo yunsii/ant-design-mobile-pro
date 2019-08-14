1742adb64278c53dc517b6e472701212613dcbd5
xieyunsheng <xieyunsheng>
2019-08-14 16:11:55 +0800

    feat: use rem

--- src/pages/User/Login.tsx
+++ src/pages/User/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, InputItem } from 'antd-mobile';
+import { List, InputItem, Toast } from 'antd-mobile';
 import { connect } from 'dva';
 import { createForm } from 'rc-form';
 import Paper from '@/components/Paper';
@@ -11,9 +11,9 @@ import styles from './Login.less';
 const logo = '/logo.svg';
 const logoStyle = {
   position: 'relative',
-  width: 48,
-  height: 48,
-  padding: 12,
+  width: '.48rem',
+  height: '.48rem',
+  padding: '.12rem',
   boxSizing: 'content-box',
   backgroundColor: 'white',
   borderRadius: '50%',
@@ -22,7 +22,7 @@ const logoStyle = {
   // backgroundSize: '48px 48px',
 }
 
-const iconStyle = { fontSize: 24, color: '#776e6e' };
+const iconStyle = { fontSize: '.24rem', color: '#776e6e' };
 
 export interface LoginProps {
   form: any;
@@ -35,8 +35,11 @@ class Login extends React.PureComponent<LoginProps> {
   handleSubmit = () => {
     const { form, dispatch } = this.props;
     form.validateFields((err, values) => {
-      if (err) return;
-      console.log(values);
+      console.log(err, values);
+      if (err) {
+        Toast.info('请输入正确的帐号密码', 1.2, undefined, false);
+        return;
+      };
       dispatch({
         type: 'login/login',
         payload: {
@@ -56,7 +59,7 @@ class Login extends React.PureComponent<LoginProps> {
     const icon = (
       <div className={styles.logoWrapper}>
         <img
-          width={48}
+          width={'.48rem'}
           src={logo}
           alt=""
           style={logoStyle as any}
@@ -66,14 +69,19 @@ class Login extends React.PureComponent<LoginProps> {
     const { form: { getFieldProps }, loading } = this.props;
 
     return (
-      <div style={{ marginTop: 'calc(50vh - 180px)', padding: '0 16px' }}>
+      <div style={{ marginTop: '2rem', padding: '0 .16rem' }}>
         <Paper icon={icon}>
-          <List style={{ marginTop: 32 }}>
+          <List style={{ marginTop: '.32rem' }}>
             <InputItem
               placeholder="请输入帐号"
               autoComplete='off'
               labelNumber={1}
-              {...getFieldProps('username')}
+              {...getFieldProps('username', {
+                rules: [{
+                  required: true,
+                  message: '请输入帐号',
+                }],
+              })}
               clear={true}
             >
               <CustomIcon type="user" style={iconStyle} />
@@ -83,7 +91,12 @@ class Login extends React.PureComponent<LoginProps> {
               placeholder="请输入密码"
               autoComplete='off'
               labelNumber={1}
-              {...getFieldProps('password')}
+              {...getFieldProps('password', {
+                rules: [{
+                  required: true,
+                  message: '请输入密码',
+                }],
+              })}
               clear={true}
               onKeyPress={this.handleKeyPress}
             >
@@ -93,7 +106,7 @@ class Login extends React.PureComponent<LoginProps> {
           <Button
             type="primary"
             loading={loading}
-            style={{ margin: '32px 4px' }}
+            style={{ margin: '.32rem .04rem' }}
             onClick={this.handleSubmit}
           >
             登录
