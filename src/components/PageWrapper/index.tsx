d49ac25b70065c555fd61e58088dafddbf5eb9db
xieyunsheng <xieyunsheng>
2019-08-20 18:57:59 +0800

    feat: fixed config

--- src/components/PageWrapper/index.tsx
+++ src/components/PageWrapper/index.tsx
@@ -1,17 +1,19 @@
-
 import React from 'react';
 import router from 'umi/router';
+import classNames from 'classnames'
 import { NavBar, Icon } from 'antd-mobile';
-import { NavBarProps } from 'antd-mobile/lib/nav-bar/PropsType'
+import { NavBarProps } from 'antd-mobile/lib/nav-bar/PropsType';
+import styles from './index.less';
 
 export interface PageWrapperProps extends NavBarProps {
   title?: any;
   backable?: boolean;
   backPath?: string;
+  fixed?: boolean;
 }
 
 export default function PageWrapper(props: PageWrapperProps) {
-  const { title, backable, backPath, children } = props;
+  const { title, backable, backPath, fixed, className, children } = props;
 
   let backableConfig: any = {
     icon: <Icon type="left" />,
@@ -26,10 +28,19 @@ export default function PageWrapper(props: PageWrapperProps) {
       <NavBar
         mode="dark"
         {...backableConfig}
+        className={classNames(fixed ? styles.fixed : '', className)}
       >
         {title}
       </NavBar>
-      {children}
+      {fixed ?
+        <div style={{ marginTop: '.45rem' }}>
+          {children}
+        </div> :
+        children}
     </>
   )
+}
+
+PageWrapper.defaultProps = {
+  fixed: true,
 }
