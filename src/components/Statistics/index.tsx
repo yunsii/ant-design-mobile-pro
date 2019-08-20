df0fd053e8be265ebada1c14ea500d51b8022579
xieyunsheng <xieyunsheng>
2019-08-20 18:57:44 +0800

    feat: update style, add default value and className

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -4,21 +4,22 @@ import styles from './index.less';
 
 type Props = {
   title: any;
-  value: number;
+  value: any;
   align?: string;
+  className?: string;
 };
 const Statistics = (props: Props) => {
-  const { title, value, align } = props;
+  const { title, value, align, className } = props;
 
   let style: React.CSSProperties = {};
   if (align === 'center') {
     style = { textAlign: 'center' };
   }
   return (
-    <>
+    <div className={className}>
       <div className={styles.item} style={style}>{title}</div>
-      <p className={styles.value} style={style}>{value}</p>
-    </>
+      <p className={styles.value} style={style}>{value || '-'}</p>
+    </div>
   );
 };
 
