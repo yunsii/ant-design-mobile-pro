f6be180e69d54969c74de19c03d68df6c82d1732
xieyunsheng <xieyunsheng>
2019-08-22 09:17:45 +0800

    feat: export FormContext

--- src/components/Form/FormContext.tsx
+++ src/components/Form/FormContext.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
 const FormContext = React.createContext<any>(null);
+export default FormContext;
 export const FormProvider = FormContext.Provider;
 export const FormConsumer = FormContext.Consumer;
