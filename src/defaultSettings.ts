e5cf9d469b4ebce33969ae3edeae900857e9442f
Yuns Xie <yuns.xie@qq.com>
2019-08-22 23:01:07 +0800

    chore: map icon

--- src/defaultSettings.ts
+++ src/defaultSettings.ts
@@ -10,5 +10,5 @@ module.exports = {
   // eg：//at.alicdn.com/t/font_1039637_btcrd5co4w.js
   // 注意：如果需要图标多色，Iconfont 图标项目里要进行批量去色处理
   // Usage: https://github.com/ant-design/ant-design-pro/pull/3517
-  iconfontUrl: '//at.alicdn.com/t/font_1347723_ewrzcepy1c6.js',
+  iconfontUrl: '//at.alicdn.com/t/font_1347723_k79qkihvbd9.js',
 };
