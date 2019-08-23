33e1fd57731c964cd2050bcd396416bca7986ba9
xieyunsheng <xieyunsheng>
2019-08-23 10:18:55 +0800

    feat: comment set viewport by setRem

--- src/global.ts
+++ src/global.ts
@@ -15,22 +15,22 @@ addScriptToHead(iconfontUrl);
 function setRem(designWidth?: number, baseFontSize?: number) {
   const _designWidth = designWidth || 750;
   const _baseFontSize = baseFontSize || 75;
-  const ua = navigator.userAgent;
-  const matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
-  const isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
-  let dpr = window.devicePixelRatio || 1;
-  if (!isIos && !(matches && +matches[1] > 534)) {
-    // 如果非iOS, 非Android4.3以上, dpr设为1;
-    dpr = 1;
-  }
-  const scale = 1 / dpr;
-  let metaEl = document.querySelector('meta[name="viewport"]');
-  if (!metaEl) {
-    metaEl = document.createElement('meta');
-    metaEl.setAttribute('name', 'viewport');
-    window.document.head.appendChild(metaEl);
-  }
-  metaEl.setAttribute('content', 'width=device-width,user-scalable=no,initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale);
+  // const ua = navigator.userAgent;
+  // const matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
+  // const isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
+  // let dpr = window.devicePixelRatio || 1;
+  // if (!isIos && !(matches && +matches[1] > 534)) {
+  //   // 如果非iOS, 非Android4.3以上, dpr设为1;
+  //   dpr = 1;
+  // }
+  // const scale = 1 / dpr;
+  // let metaEl = document.querySelector('meta[name="viewport"]');
+  // if (!metaEl) {
+  //   metaEl = document.createElement('meta');
+  //   metaEl.setAttribute('name', 'viewport');
+  //   window.document.head.appendChild(metaEl);
+  // }
+  // metaEl.setAttribute('content', 'width=device-width,user-scalable=no,initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale);
   document.documentElement.style.fontSize = document.documentElement.clientWidth / (_designWidth / _baseFontSize) + 'px';
 };
 
