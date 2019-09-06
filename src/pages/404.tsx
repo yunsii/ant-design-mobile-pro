6f41e3274badb496e94e0dd327ba65fdbeb3afca
Yuns Xie <yuns.xie@qq.com>
2019-09-06 22:25:53 +0800

    feat: update style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,16 @@
-import { Result } from 'antd-mobile';
+import { Result, Icon } from 'antd-mobile';
 import React from 'react';
 import router from 'umi/router';
 
 const NoFoundPage: React.FC<{}> = () => (
   <Result
+    img={<Icon type="cross-circle-o" className="spe" style={{ fill: '#F13642', width: 60, height: 60 }} />}
     title="404"
     message="Sorry, the page you visited does not exist."
     buttonText="Back Home"
     buttonType="primary"
     onButtonClick={() => router.push('/')}
+    style={{ height: '100vh', paddingTop: 'calc(50vh - 3rem)' }}
   />
 );
 
