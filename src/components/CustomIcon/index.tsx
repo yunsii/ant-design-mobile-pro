7f0eeac8f3c5b1781044cf7dabc3a0e713edbaca
Yuns Xie <yuns.xie@qq.com>
2019-11-12 12:22:12 +0800

    feat: icon size

--- src/components/CustomIcon/index.tsx
+++ src/components/CustomIcon/index.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import styles from './index.less';
+import { pxToRem } from '@/utils/customUtils'
 
 type Props = {
   type: string;
+  size?: number;
   style?: React.CSSProperties;
   className?: string;
   onClick?: () => void;
 };
 const CustomIcon = (props: Props) => {
-  const { type, className, ...rest } = props;
+  const { type, className, style, size, ...rest } = props;
+  const sizeStyle = size ? {
+    width: pxToRem(size),
+    height: pxToRem(size)
+  } : {};
+
   return (
-    <svg className={classNames(styles.icon, className)} aria-hidden="true" {...rest}>
+    <svg className={classNames(styles.icon, className)} aria-hidden="true" style={{ ...sizeStyle, ...style }} {...rest}>
       <use xlinkHref={`#icon-${type}`} />
     </svg>
   );
