55399aa64ef678ccccde45b102eac278d6e3baf8
Yuns Xie <yuns.xie@qq.com>
2019-11-12 11:39:50 +0800

    refactor && feat: style && className and onClick

--- src/components/CustomIcon/index.tsx
+++ src/components/CustomIcon/index.tsx
@@ -1,14 +1,18 @@
-// @flow 
 import * as React from 'react';
+import classNames from 'classnames';
+import styles from './index.less';
+
 type Props = {
   type: string;
   style?: React.CSSProperties;
+  className?: string;
+  onClick?: () => void;
 };
 const CustomIcon = (props: Props) => {
-  const { type, ...rest } = props;
+  const { type, className, ...rest } = props;
   return (
-    <svg className="icon" aria-hidden="true" {...rest}>
-      <use xlinkHref={`#icon-${type}`}/>
+    <svg className={classNames(styles.icon, className)} aria-hidden="true" {...rest}>
+      <use xlinkHref={`#icon-${type}`} />
     </svg>
   );
 };
