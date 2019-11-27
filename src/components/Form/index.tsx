673c95e1233dcd6bd9105d73b4ef7ca55e3af346
Yuns Xie <yuns.xie@qq.com>
2019-11-27 18:24:56 +0800

    feat && refactor: datetime and footerErrors,  required mark && error text color

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,23 +1,33 @@
 import React, { useContext } from 'react';
-import { List, InputItem, Picker, Button, TextareaItem } from 'antd-mobile';
+import { List, InputItem, Picker, Button, TextareaItem, Toast, DatePicker } from 'antd-mobile';
 import _values from 'lodash/values';
+import _get from 'lodash/get';
+import _find from 'lodash/find';
 import { ButtonProps } from 'antd-mobile/lib/button'
 import { InputItemProps } from 'antd-mobile/lib/input-item'
 import { TextareaItemProps } from 'antd-mobile/lib/textarea-item'
 import { PickerPropsType } from 'antd-mobile/lib/picker/PropsType'
 import FormContext, { FormProvider } from './FormContext';
 import CustomImagePicker, { ImagePickerProps } from './components/CustomImagePicker';
+import styles from './index.less';
+
+export interface WrappedImagePickerProps extends ImagePickerProps {
+  extra?: string;
+}
 
 export type ComponentType =
   | "custom"
   | "picker"
   | "picture"
+  | "date"
+  | "time"
+  | "datetime"
   | "textarea"
   | "string"
 
 export type ComponentProps =
   | PickerPropsType
-  | ImagePickerProps
+  | WrappedImagePickerProps
   | TextareaItemProps
   | InputItemProps
 
@@ -26,9 +36,12 @@ const renderInputComponent = (form: any) => (
   label: any,
   field: string,
   componentProps: any,
+  component?: JSX.Element,
 ) => {
   const { setFieldsValue } = form;
   switch (type) {
+    case 'custom':
+      return component;
     case 'picker':
       return (
         <Picker
@@ -41,21 +54,27 @@ const renderInputComponent = (form: any) => (
         </Picker>
       )
     case 'picture':
-      const WrappedImagePicker = React.forwardRef((props, ref) => {
+      const WrappedImagePicker = React.forwardRef((props: WrappedImagePickerProps, ref) => {
+        const { extra, ...rest } = props;
         return (
           <List.Item>
             <p style={{ margin: 0 }}>{label}</p>
             <CustomImagePicker
               onImageClick={(index, fs) => console.log(index, fs)}
-              {...props}
+              {...rest}
               ref={ref as any}
             />
+            <p className={styles['picture-extra']} style={{ margin: 0 }}>{extra}</p>
           </List.Item>
         )
       })
       return <WrappedImagePicker />
     case 'textarea':
-      return <TextareaItem title={label} />
+      return <TextareaItem title={label} rows={2} placeholder='请输入' />
+    case 'date':
+    case 'time':
+    case 'datetime':
+      return <DatePicker mode={type}><List.Item arrow="horizontal">{label}</List.Item></DatePicker>
     case 'string':
     default:
       return (
@@ -96,39 +115,45 @@ export interface ItemConfig {
 
 export interface FormProps {
   header?: any;
-  items: ItemConfig[];
+  items?: ItemConfig[];
   errorsFooter?: boolean;
   onSubmit?: (fieldsValue: any) => void;
   buttonText?: string | null;
   buttonProps?: ButtonProps;
   style?: React.CSSProperties;
+  className?: string;
+  footerErrors?: boolean;
+  hideRequiredMark?: boolean;
 }
 
 function Form(props: FormProps) {
   const {
     header,
-    items,
+    items = [],
     errorsFooter = true,
     onSubmit,
     style,
     buttonText = '确定',
     buttonProps,
+    footerErrors = true,
+    className,
+    hideRequiredMark = false,
   } = props;
   const form = useContext(FormContext);
   const { getFieldsError } = form;
 
   const setFormItems = (_items: ItemConfig[]) => {
-    const { getFieldProps } = form;
+    const { getFieldProps, getFieldError } = form;
     return _items.map(item => {
       const { type = 'string', label, field, componentProps, component, fieldProps } = item;
 
-      // const error = getFieldError(field);
-      // const errorProps = error ? {
-      //   error,
-      //   onErrorClick: () => Toast.info(error[0]),
-      // } : {};
+      const error = getFieldError(field);
+      const errorProps = !footerErrors && error ? {
+        error,
+        onErrorClick: () => Toast.info(error[0]),
+      } : {};
 
-      const _fieldProps = type !== 'custom' ? {
+      const injectfieldProps = type !== 'custom' ? {
         ...getFieldProps(
           field,
           {
@@ -138,16 +163,31 @@ function Form(props: FormProps) {
         ),
       } : {};
 
-      return type === 'custom' && component ? React.cloneElement(component, { key: field }) : (
+      const renderComponent = renderInputComponent(form)(
+        type,
+        label,
+        field,
+        componentProps,
+        component,
+      );
+
+      const renderItem = renderComponent && (
         React.cloneElement(
-          renderInputComponent(form)(type, label, field, componentProps),
+          renderComponent,
           {
             ...componentProps as any,
-            ..._fieldProps,
+            ...injectfieldProps,
+            ...errorProps,
             key: field,
           }
         )
       )
+
+      return !hideRequiredMark && !!_find(_get(fieldProps, 'rules', []), { required: true }) ? (
+        <div key={field} className={styles.required}>
+          {renderItem}
+        </div>
+      ) : renderItem;
     }).filter(item => item) as JSX.Element[];
   }
 
@@ -180,13 +220,13 @@ function Form(props: FormProps) {
   if (header) { listProps.renderHeader = () => header; }
 
   const errors = _values(getFieldsError(items.map(item => item.field))).filter(item => item);
-  if (errorsFooter && errors.length) {
-    listProps.renderFooter = () => <span style={{ color: '#ff5f0f' }}>{errors.join('，')}</span>;
+  if (footerErrors && errorsFooter && errors.length) {
+    listProps.renderFooter = () => <span className={styles['error-text']}>{errors.join('，')}</span>;
   }
 
   return (
     <>
-      <List style={style} {...listProps}>
+      <List style={style} className={className} {...listProps}>
         {setFormItems(items)}
         {buttonText ?
           <List.Item>
