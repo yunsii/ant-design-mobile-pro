7a771c53791980a23c2c8f5d84cacdface41fc18
Yuns Xie <yuns.xie@qq.com>
2019-11-28 12:17:19 +0800

    feat: renderAsyncData

--- src/utils/reactUtils.tsx
+++ src/utils/reactUtils.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Result } from 'antd-mobile';
 
 export function withProps(props: any) {
   return function injectProps(WrappedComponent: JSX.Element) {
     return React.cloneElement(WrappedComponent, props)
   }
+}
+
+export function renderAsyncData<T>(loading: boolean, data: T | T[], element: any) {
+  if (loading) {
+    return <Result message='加载中...' />
+  }
+  if ((Array.isArray(data) && !data.length) || !Object.keys(data).length) {
+    return <Result message='暂无数据' />
+  }
+  return element;
 }
