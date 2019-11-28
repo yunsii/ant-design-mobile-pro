0a636915acc97ab73f24f6ce25dec35d93f7c46b
Yuns Xie <yuns.xie@qq.com>
2019-11-28 11:33:28 +0800

    feat: Paper's color and onClick

--- src/components/Paper/index.tsx
+++ src/components/Paper/index.tsx
@@ -1,42 +1,73 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './index.less';
+
+const colors: any = [
+  'pink',
+  'red',
+  'yellow',
+  'orange',
+  'cyan',
+  'green',
+  'blue',
+  'purple',
+  'geekblue',
+  'magenta',
+  'volcano',
+  'gold',
+  'lime',
+];
 
 export interface PaperProps {
   full?: boolean;
   icon?: React.ReactNode;
+  color?: 'pink' | 'red' | 'yellow' | 'orange' | 'cyan' | 'green' | 'blue' | 'purple' | 'geekblue' | 'magenta' | 'volcano' | 'gold' | 'lime' | string;
   children?: any;
   /** paper wrapper className */
   clearPadding?: boolean;
+  onClick?: () => void;
 }
 
 export default function Paper(props: PaperProps) {
-  const { children, full, icon, clearPadding } = props;
-  let style = {};
-  if (full) {
-    style = { ...style, borderRadius: 0, margin: 0 };
-  }
+  const { children, full, icon, clearPadding, color, ...rest } = props;
 
-  let renderIcon: any = null;
-  if (icon) {
-    renderIcon = <div className={styles.icon}>{icon}</div>;
-    style = {
-      ...style,
+  const setRenderIcon = () => icon && <div className={styles.icon}>{icon}</div>;
 
-      marginTop: '.3rem',
-      paddingTop: '1rem',
-    };
-  }
-  if (clearPadding) {
-    style = {
-      ...style,
-
-      paddingLeft: 0,
-      paddingRight: 0,
+  const setStyle = () => {
+    let style: React.CSSProperties = {};
+    if (full) {
+      style = { ...style, borderRadius: 0, margin: 0 };
     }
+    if (setRenderIcon()) {
+      style = {
+        ...style,
+        marginTop: '.3rem',
+        paddingTop: '1rem',
+      };
+    }
+    if (clearPadding) {
+      style = {
+        ...style,
+        paddingLeft: 0,
+        paddingRight: 0,
+      }
+    }
+    if (!colors.includes(color)) {
+      style = {
+        ...style,
+        background: color,
+      }
+    }
+    return style;
   }
+
   return (
-    <div className={styles.paper} style={style}>
-      {renderIcon}
+    <div
+      {...rest}
+      className={classNames(styles.paper, colors.includes(color) ? styles[`paper-color-${color}`] : '')}
+      style={setStyle()}
+    >
+      {setRenderIcon()}
       {children}
     </div>
   );
