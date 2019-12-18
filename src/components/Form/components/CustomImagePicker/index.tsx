71d32b1a74eb4fb3ebc372f5a27b2df457086e77
Yuns Xie <yuns.xie@qq.com>
2019-12-18 14:48:28 +0800

    feat: preview image and filesCountLimit onChange

--- src/components/Form/components/CustomImagePicker/index.tsx
+++ src/components/Form/components/CustomImagePicker/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ImagePicker } from 'antd-mobile';
 import { ImagePickerPropTypes } from 'antd-mobile/lib/image-picker';
+import Image from '@/components/Image';
+import PopupModal from '@/components/custom-antd-mobile/PopupModal';
 
 export interface ImagePickerFile {
   file: File;
@@ -13,25 +15,61 @@ export interface ImagePickerProps extends Omit<ImagePickerPropTypes, 'onChange'>
   filesCountLimit?: number;
   onChange?: (files: ImagePickerFile[]) => void;
 }
-export default class extends React.Component<ImagePickerProps> {
+interface ImagePickerState {
+  visible: boolean;
+  previewUrl: string;
+}
+export default class extends React.Component<ImagePickerProps, ImagePickerState> {
+
+  state = {
+    visible: false,
+    previewUrl: '',
+  }
+
   /**
    * remove with index
    */
   onChange = (files: ImagePickerFile[], type: 'add' | 'remove', index: number) => {
-    const { onChange = () => { } } = this.props;
+    const { onChange = () => { }, filesCountLimit } = this.props;
     console.log(files, type, index);
-    onChange(files);
+    onChange(filesCountLimit ? files.slice(0, filesCountLimit) : files);
   }
 
   render() {
     const { onChange, filesCountLimit = 1, disabled, ...rest } = this.props;
+    const { visible, previewUrl } = this.state;
     return (
-      <ImagePicker
-        disableDelete={disabled}
-        onChange={this.onChange as any}
-        selectable={!rest.files || rest.files.length < (isNaN(filesCountLimit) ? 1 : filesCountLimit)}
-        {...rest}
-      />
+      <>
+        <ImagePicker
+          disableDelete={disabled}
+          onChange={this.onChange as any}
+          selectable={!rest.files || rest.files.length < (isNaN(filesCountLimit) ? 1 : filesCountLimit)}
+          onImageClick={(index, files) => {
+            if (index !== undefined && files) {
+              // console.log(index, files);
+              this.setState({
+                visible: true,
+                previewUrl: (files[index] as any).url,
+              });
+            }
+          }}
+          {...rest}
+        />
+        <PopupModal
+          visible={visible}
+          onClose={() => { this.setState({ visible: false }) }}
+          footer={[
+            {
+              text: '关闭',
+              onPress: () => {
+                this.setState({ visible: false });
+              }
+            }
+          ]}
+        >
+          <Image src={previewUrl} />
+        </PopupModal>
+      </>
     );
   }
 }
