acf19c441b09927a20864939c391d0a3e4495d76
Yuns Xie <yuns.xie@qq.com>
2020-02-07 15:57:17 +0800

    feat: noTitle style

--- src/components/custom-antd-mobile/CustomResult/index.tsx
+++ src/components/custom-antd-mobile/CustomResult/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Result } from 'antd-mobile';
-import { ResultProps } from 'antd-mobile/lib/result'
-import _repeat from 'lodash/repeat';
+import { ResultProps } from 'antd-mobile/lib/result';
+import classNames from 'classnames';
+import styles from './index.less';
 
-export const CustomResult: React.FC<ResultProps> = ({...props}) => {
-  return <Result style={{ border: 'unset' }} {...props} />
+export const CustomResult: React.FC<ResultProps> = ({ className, ...rest }) => {
+  return (
+    <Result
+      style={{ border: 'unset' }}
+      className={classNames(!rest.title && styles.noTitle, className)}
+      {...rest}
+    />
+  )
 }
 
 export default CustomResult;
